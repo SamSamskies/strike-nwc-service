aa648cbd670f5e61232d10a2308e7483b1b6b4a9
Sam Samskies <samsamskies@gmail.com>
2024-05-10 15:39:23 -0500

    fix connection string

--- src/scripts/printNwcConnectionString.js
+++ src/scripts/printNwcConnectionString.js
@@ -2,5 +2,5 @@ const { getNwcServicePubkey } = require("../utils");
 const { RELAY_URI, NWC_CONNECTION_SECRET } = require("../constants");
 
 console.log(
-  `nostr+walletconnect:${getNwcServicePubkey()}?${RELAY_URI}&secret=${NWC_CONNECTION_SECRET}`,
+  `nostr+walletconnect:${getNwcServicePubkey()}?relay=${RELAY_URI}&secret=${NWC_CONNECTION_SECRET}`,
 );
